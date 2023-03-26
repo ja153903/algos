78d1d91a1c3449d6a18e46b6761251725ed9dea9
ja153903 <jaimeabbariao@gmail.com>
2023-03-25 22:51:46 -0400

    feat: adding dp and interval questions

---
+++ src/neetcode/intervals/lp57_insert_interval.py
@@ -0,0 +1,7 @@
+class Solution:
+    def insert(
+        self, intervals: list[list[int]], new_interval: list[int]
+    ) -> list[list[int]]:
+        # if we're trying to insert an interval into an existing set of intervals
+        # we should first iterate first until
+        pass
