b303a26daf10c3c39789510d110bd0c45758d8a3
ja153903 <jaimeabbariao@gmail.com>
2023-03-28 09:11:00 -0400

    feat: updating with more solutions

---
+++ src/neetcode/arrays_and_hashing/lp1_two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+
+            seen[num] = i
+
+        return []
