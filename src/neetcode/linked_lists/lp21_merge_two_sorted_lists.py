250dee9f307e7b6279b7124b699c12609f6b5122
ja153903 <jaimeabbariao@gmail.com>
2023-03-29 22:20:27 -0400

    feat: updating with some stuff

--- src/neetcode/linked_lists/lp21_merge_two_sorted_lists.py
+++ src/neetcode/linked_lists/lp21_merge_two_sorted_lists.py
@@ -4,11 +4,11 @@ from src.data_structures.linked_list import ListNode
 class Solution:
     def mergeTwoLists(self, a: ListNode | None, b: ListNode | None) -> ListNode | None:
         match (a, b):
-            case (an, None):
+            case (an, None) if an is not None:
                 return an
-            case (None, bn):
+            case (None, bn) if bn is not None:
                 return bn
-            case (an, bn):
+            case (an, bn) if an is not None and bn is not None:
                 if an.val < bn.val:
                     an.next = self.mergeTwoLists(an.next, bn)
                     return an
