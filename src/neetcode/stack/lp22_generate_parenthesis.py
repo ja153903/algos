e6b9dbb51944c85cfe2d794380e5735533c4f0d3
ja153903 <jaimeabbariao@gmail.com>
2023-04-02 17:56:59 -0400

    feat: updating with a stack solution

--- src/neetcode/stack/lp22_generate_parenthesis.py
+++ src/neetcode/stack/lp22_generate_parenthesis.py
@@ -1,4 +1,24 @@
-# TODO: Solve this problem
+from collections import deque, namedtuple
+
+Node = namedtuple("Node", ["path", "left", "right"])
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        pass
+        result = []
+        queue = deque()
+        queue.append(Node("(", n - 1, n))
+
+        while queue:
+            front = queue.popleft()
+
+            if front.left == 0 and front.right == 0:
+                result.append(front.path)
+            else:
+                if front.left > 0:
+                    queue.append(Node(f"{front.path}(", front.left - 1, front.right))
+
+                if front.right > front.left:
+                    queue.append(Node(f"{front.path})", front.left, front.right - 1))
+
+        return result
